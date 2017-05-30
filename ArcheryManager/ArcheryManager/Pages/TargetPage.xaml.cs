13f9a3bb32460631c28461f0a937a2e194177d2f
tibo <pillon.tibo@gmail.com>
2017-05-31 01:28:31 +0200

    remove useless using

--- ArcheryManager/ArcheryManager/Pages/TargetPage.xaml.cs
+++ ArcheryManager/ArcheryManager/Pages/TargetPage.xaml.cs
@@ -1,11 +1,4 @@
-﻿using ArcheryManager.Behaviors;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ArcheryManager.Pages
