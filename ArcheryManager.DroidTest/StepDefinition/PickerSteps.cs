e359a8580252383f537a203047ba223edc1a00fa
tibo <pillon.tibo@gmail.com>
2017-07-08 16:00:52 +0200

    remove conflicts

--- ArcheryManager.DroidTest/StepDefinition/PickerSteps.cs
+++ ArcheryManager.DroidTest/StepDefinition/PickerSteps.cs
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Linq;
-=======
-﻿using System;
->>>>>>> test on the time selector
 using TechTalk.SpecFlow;
 
 namespace ArcheryManager.DroidTest.StepDefinition
@@ -13,7 +8,6 @@ namespace ArcheryManager.DroidTest.StepDefinition
         [When(@"je selectionne (.*) dans le picker")]
         public void QuandJeSelectionneDansLePicker(int p0)
         {
-<<<<<<< HEAD
             TestSetting.App.ScrollDownTo(e => e.Text(p0.ToString()));
             TestSetting.App.Tap(e => e.Text(p0.ToString()));
         }
@@ -22,15 +16,6 @@ namespace ArcheryManager.DroidTest.StepDefinition
         public void AlorsUnPickerSOuvre()
         {
             TestSetting.App.WaitForElement(e => e.Id("select_dialog_listview"));
-=======
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"un picker s'ouvre avec (.*) de selectionné")]
-        public void AlorsUnPickerSOuvreAvecDeSelectionne(int p0)
-        {
-            ScenarioContext.Current.Pending();
->>>>>>> test on the time selector
         }
     }
 }
